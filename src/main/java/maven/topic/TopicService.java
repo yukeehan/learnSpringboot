6c52ab0f828a13cfc490c5f2266d095cd87417f3
yukeehan <yukeehan@gmail.com>
2019-03-26 10:45:23 -0400

    learn spring boot

---
+++ src/main/java/maven/topic/TopicService.java
@@ -0,0 +1,43 @@
+package maven.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("spring2", "Spring Framework2", "Spring Framework Description2"),
+			new Topic("spring3", "Spring Framework3", "Spring Framework Description3")
+			));
+	
+	public List<Topic> getAllTopisc() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
